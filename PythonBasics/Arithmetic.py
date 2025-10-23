0306b7d330cd9e1f7ea41ee559d23448770eaf87
satyanand singh <satyanand_singh@live.com>
2025-10-24 05:17:24 +0530

    committing arithmetic basic

---
+++ PythonBasics/Arithmetic.py
@@ -0,0 +1,22 @@
+x = 23
+y = 46
+z = x*y
+z1 = x**y #this is way of deffing exponent that is 23 base to power 46
+print(z,z1)
+
+a = 10
+b = 3
+c = a/b
+c1 = a//b
+
+print(c,c1)
+
+c2 = a%b
+print(c2)
+
+d = (a*b)**4
+
+print(d)
+
+num = input('num is ') # input function return string as type
+print(int(num)-5)
