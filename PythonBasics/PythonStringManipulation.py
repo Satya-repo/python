e679f31140708724dcda4863c9e660160d53bed8
satyanand singh <satyanand_singh@live.com>
2025-10-24 05:34:49 +0530

    string basics

---
+++ PythonBasics/PythonStringManipulation.py
@@ -0,0 +1,17 @@
+# PYTHON STRING MANIPULATION - TOP 15 MOST USED METHODS
+# =====================================================
+
+print("PYTHON STRING MANIPULATION - TOP 15 METHODS")
+print("="*6)
+
+
+# Sample strings for demonstration
+text = "  Hello World!  "
+
+
+print(f"Original text: {text}")
+print(f"Original text: {text.strip()}")
+print(f"Original text: {text.capitalize()}")
+print(f"Original text: {text.upper()}")
+print(f"Original text: {text.lower()}")
+print(f"Original text: {text.replace('Hello','helllloooooo')}")
