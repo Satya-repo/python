745738a3ee50802ced786c8caa9d510d6b9ea60e
satyanand singh <satyanand_singh@live.com>
2025-10-24 10:00:17 +0530

    basic condition

---
+++ PythonBasics/conditionOper.py
@@ -0,0 +1,23 @@
+x = 5
+y = 3
+z = 7
+
+print(x!=y)
+print(z != x+y)
+
+print(z<= x+y)
+print(x+y>= z)
+
+name = 'satya'
+
+if name == 'tanima':
+    print(name)
+elif name == 'parth':
+    print(name)
+else:
+    print(name)
+
+if 2+3> 2**3:
+    print(True)
+else:
+    print(False)
